64194f63166fcb68c63357aa5488d4f54edd961e
isguliyev <isguliyev24@gmail.com>
2026-02-22 19:17:53 +0400

    chore: add command-line argument sum calculator

---
+++ 0x01-java-structure-decision-repetition/Sum.java
@@ -0,0 +1,20 @@
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        try {
+            for (int i = 0; i < args.length; i++) {
+                sum += Integer.parseInt(args[i]);
+            }
+        } catch (NumberFormatException numberFormatException) {
+            System.err.println(numberFormatException.toString());
+            System.exit(1);
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + (i < args.length - 1 ? " + " : " = "));
+        }
+
+        System.out.println(sum);
+    }
+}
