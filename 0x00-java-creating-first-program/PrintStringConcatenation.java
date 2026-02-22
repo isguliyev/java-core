e4973a942522d55fce1e55fca74615c2c51d8824
isguliyev <isguliyev24@gmail.com>
2026-02-22 15:51:05 +0400

    refactor: simplify concatenation logic by removing extra variable

--- 0x00-java-creating-first-program/PrintStringConcatenation.java
+++ 0x00-java-creating-first-program/PrintStringConcatenation.java
@@ -2,8 +2,7 @@ public class PrintStringConcatenation {
     public static void main(String[] args) {
         final String text1 = "Hello, ";
         final String text2 = "World!";
-        final String completeText = text1 + text2;
 
-        System.out.printf("\"%s\" + \"%s\" -> \"%s\"\n", text1, text2, completeText);
+        System.out.printf("\"%s\" + \"%s\" -> \"%s\"\n", text1, text2, text1 + text2);
     }
 }
