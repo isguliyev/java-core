9ccee7ff7cda6899cc72c83adf87607ea195c01d
isguliyev <isguliyev24@gmail.com>
2026-02-23 19:51:02 +0400

    chore: implement people count by position task

---
+++ 0x15-java-streams-collectors-and-optional-practice-exercises/people-count-position/PeopleQuery.java
@@ -0,0 +1,29 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class PeopleQuery {
+    public static Map<String, Set<Person>> getPeopleGroupedByPositionInReverseOrder(
+        List<Person> people
+    ) {
+        return people.stream().collect(
+            Collectors.groupingBy(
+                Person::getPosition,
+                () -> new TreeMap<String, Set<Person>>(Comparator.reverseOrder()),
+                Collectors.toCollection(
+                    () -> new TreeSet<>(Comparator.comparing(Person::getName))
+                )
+            )
+        );
+    }
+
+    public static Map<String, Long> getPeopleCountByPosition(List<Person> people) {
+        return people.stream().collect(
+            Collectors.groupingBy(Person::getPosition, Collectors.counting())
+        );
+    }
+}
