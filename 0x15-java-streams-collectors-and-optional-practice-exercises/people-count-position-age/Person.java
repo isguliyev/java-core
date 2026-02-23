3ba56fe8cc47ffc7b69ab8f855fa9de4c9030e90
isguliyev <isguliyev24@gmail.com>
2026-02-23 19:54:37 +0400

    chore: implement people count by age and position task

---
+++ 0x15-java-streams-collectors-and-optional-practice-exercises/people-count-position-age/Person.java
@@ -0,0 +1,73 @@
+public class Person {
+    private int code;
+    private String name;
+    private String position;
+    private int age;
+    private double salary;
+
+    public Person(
+        int code,
+        String name,
+        String position,
+        int age,
+        double salary
+    ) {
+        setCode(code);
+        setName(name);
+        setPosition(position);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "[%d] %s %s %d $ %.2f",
+            this.code,
+            this.name,
+            this.position,
+            this.age,
+            this.salary
+        );
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPosition() {
+        return this.position;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
