eb79e80bf829a785a4dd595df54d3fd8e479ce9a
isguliyev <isguliyev24@gmail.com>
2026-02-23 22:29:22 +0400

    refactor: remove redundant variable and use printf

--- 0x17-java-introduction-to-software-testing/email/Program.java
+++ 0x17-java-introduction-to-software-testing/email/Program.java
@@ -1,15 +1,18 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.print("Valid Email Test - ");
-        boolean isValidEmail = EmailTest.test_email_with_at_symbol();
-        System.out.println(isValidEmail);
+        System.out.printf(
+            "Valid Email Test - %b\n",
+            EmailTest.test_email_with_at_symbol()
+        );
 
-        System.out.print("Email Test Without @ Symbol - ");
-        isValidEmail = EmailTest.test_email_without_at_symbol();
-        System.out.println(isValidEmail);
+        System.out.printf(
+            "Email Test Without @ Symbol - %b\n",
+            EmailTest.test_email_without_at_symbol()
+        );
 
-        System.out.print("Email Test With More Than 50 Characters - ");
-        isValidEmail = EmailTest.test_email_over_50_characters();
-        System.out.println(isValidEmail);
+        System.out.printf(
+            "Email Test With More Than 50 Characters - %b\n",
+            EmailTest.test_email_over_50_characters()
+        );
     }
 }
